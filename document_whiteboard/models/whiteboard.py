5bc6d14761d4d88d2876d3ce168f6a4ca6a67918
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-02-25 10:52:24 +0100

    Update whiteboard.py

--- document_whiteboard/models/whiteboard.py
+++ document_whiteboard/models/whiteboard.py
@@ -5,9 +5,7 @@ from odoo.exceptions import ValidationError
 class Whiteboard(models.Model):
     _inherit = 'ir.attachment'
     
+    type = fields.Selection([('url', 'URL'), ('binary', 'File')],
+        string='Type', required=True, default='binary', change_default=True,
+        help="You can either upload a file from your computer or copy/paste an internet link to your file.")
     
-
-   
-
-
-
