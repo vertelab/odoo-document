a230d8a280ab27da040a7773eb3ebb97cd786704
iammiracle <miracleaayodele@gmail.com>
2022-04-04 20:37:18 +0100

    document whiteboard url

--- document_whiteboard_url/wizard/document_url.py
+++ document_whiteboard_url/wizard/document_url.py
@@ -20,3 +20,23 @@ class WhiteboardWizard(models.TransientModel):
             if not whiteboard_base_url:
                 ValidationError(_("No WhiteBoard URL Defined"))
             self.url = f"{whiteboard_base_url}/?whiteboardid={uuid.uuid4()}"
+
+    def action_add_url(self):
+        """Adds the URL with the given name as an ir.attachment record."""
+        if not self.env.context.get("active_model"):
+            return
+        attachment_obj = self.env["ir.attachment"]
+        for form in self:
+            url = parse.urlparse(form.url)
+            if not url.scheme:
+                url = parse.urlparse("{}{}".format("http://", form.url))
+            for active_id in self.env.context.get("active_ids", []):
+                attachment = {
+                    "name": form.name,
+                    "type": "whiteboard" if self.is_whiteboard_url else "url",
+                    "url": url.geturl(),
+                    "res_id": active_id,
+                    "res_model": self.env.context["active_model"],
+                }
+                attachment_obj.create(attachment)
+        return {"type": "ir.actions.act_window_close"}
