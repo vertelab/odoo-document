478d2905fb617071ff0007b5352d1899c9ce0c6c
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-07 11:28:45 +0200

    Update __manifest__.py

--- dms_event/__manifest__.py
+++ dms_event/__manifest__.py
@@ -34,8 +34,7 @@
     'contributor': '',
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-document',
-    'author': "Vertel AB",
-    'website': "https://www.vertel.se",
+
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
