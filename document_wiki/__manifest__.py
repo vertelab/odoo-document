5a64d70724012b5dfeca950e2d8dd02a78a0d497
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-07 10:59:59 +0200

    Update __manifest__.py

--- document_wiki/__manifest__.py
+++ document_wiki/__manifest__.py
@@ -1,15 +1,39 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    'name': 'Document Wiki',
+    'name': 'Document: Document Wiki',
     'version': '14.1.0.0.0',
-    'summary': 'Adds sub page to page',
-    'category': '',
-    'description': """
-        Adds sub page to page
-    """,
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Adds sub page to page.',
+    'category': 'Technical',
+    'description': 'Adds sub page to page.',
+    #'sequence': '1',
     'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-document/document_wiki',
+    'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
-    'website': 'https://www.vertel.se',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-document',
     'depends': ['website'],
     'data': [
         'security/ir.model.access.csv',
@@ -24,3 +48,4 @@
     ],
     'installable': True,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
