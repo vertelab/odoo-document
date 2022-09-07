8b6fd5fb046eb4278ee6754fde41e987bf7094b0
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-07 10:34:29 +0200

    Update __manifest__.py

--- document_signatures/__manifest__.py
+++ document_signatures/__manifest__.py
@@ -1,20 +1,43 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    'name': "Document Signatures",
-
-    'summary': """
-        Adds multi aproval functionality using bankid for sale orders""",
-
-    'description': """
-        Adds multi aproval functionality using bankid for sale orders
-    """,
-
-    'author': "Vertel AB",
-    'website': "www.vertel.se",
+    'name': 'Document: Document Signatures',
+    'version': '14.0.1.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Adds multi aproval functionality using bank-id for sale orders.',
+    'category': 'Accounting',
+    'description': 'Adds multi aproval functionality using bank-id for sale orders.',
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-document/document_signatures',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-document',
 
     # Categories can be used to filter modules in modules listing
-    'category': 'document',
-    'version': '14.0.0.1',
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'rest_base', 'rest_signport', 'res_user_groups_skogsstyrelsen', 'partner_ssn', 'dms'],
@@ -31,3 +54,4 @@
         'views/dms_portal_templates.xml',
     ],
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
