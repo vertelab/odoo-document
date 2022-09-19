ae433fa28bc05a6e8432f1718678b070d401ee47
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-19 16:34:21 +0200

    Update __manifest__.py

--- filestore_upload/__manifest__.py
+++ filestore_upload/__manifest__.py
@@ -25,7 +25,9 @@
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Upload large files with SFTP.',
     'category': 'Technical',
-    'description': 'Upload large files with SFTP.',
+    'description': """
+    Upload large files with SFTP.
+    """,
     #'sequence': '1',
     'author': 'Vertel AB',
     'website': 'https://vertel.se/apps/odoo-document/filestore_upload',
