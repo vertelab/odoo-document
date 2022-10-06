a0c045562ae44c574bad6ca62ca5452739fa597f
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-06 16:41:34 +0200

    Update __manifest__.py

--- dms_hierarchy/__manifest__.py
+++ dms_hierarchy/__manifest__.py
@@ -1,12 +1,41 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    'name': "DMS: DMS Hierarchy",
-    'summary': """DMS Hierarchy""",
-    'description': """DMS Hierarchy""",
-    'author': "Vertel AB",
-    'website': "https://www.vertel.se",
-    'category': 'dms',
-    'version': '14.0.0.1.0',
+    'name': 'Document: DMS Hierarchy',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'DMS Hierarch',
+    'category': 'Technical',
+    'description': """
+    DMS Hierarch
+    """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-document/dms_hierarchy',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-document',
     'depends': [
         'dms',
     ],
@@ -14,3 +43,4 @@
     ],
     'auto_install': True,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
