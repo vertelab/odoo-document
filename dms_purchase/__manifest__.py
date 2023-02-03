4bb36a0fd83fe4bef17be9f78415b1fcfc957f16
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-02-03 16:14:07 +0100

    Update __manifest__.py

--- dms_purchase/__manifest__.py
+++ dms_purchase/__manifest__.py
@@ -40,10 +40,7 @@
     # for the full list
 
     # any module necessary for this one to work correctly
-    'depends': [
-        'dms',
-        'purchase',
-    ],
+    'depends': ['dms','purchase',],
 
     # always loaded
     'data': [
