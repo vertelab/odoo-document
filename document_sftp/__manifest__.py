abbf3834e2a33c6b3e6beb6ab186089ee8121eb6
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-03-07 07:51:47 +0100

    Update __manifest__.py
    spelling error

--- document_sftp/__manifest__.py
+++ document_sftp/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Document: Document with SFTP',
+    'name': 'Document: SFTP',
     'version': '14.0.1.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Access your documents via SFTP.',
