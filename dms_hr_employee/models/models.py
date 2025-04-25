b0bacc18a2d7de84e83fedab7c0cf85e7983dd42
marcuszxc <marcus.karlsson@vertel.se>
2025-04-25 10:00:42 +0000

    dms_hr_employee: Ported from 14 to 18

---
+++ dms_hr_employee/models/models.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
+
+class Event(models.Model):
+    _inherit = "hr.employee"
+    doc_count = fields.Integer(compute='_compute_attached_docs_count', string="Doc count")
+
+    def _compute_attached_docs_count(self):
+        Document = self.env['dms.file']
+        for employee in self:
+            employee.doc_count = Document.search_count([
+                '&',
+                ('res_model', '=', 'hr.employee'), ('res_id', '=', employee.id),
+            ])
+
+    def dms_tree_view(self):
+            action = self.env['ir.actions.act_window']._for_xml_id('dms.action_dms_file')
+            action['domain'] = str([
+                '&',
+                ('res_model', '=', 'hr.employee'),
+                ('res_id', 'in', self.ids),
+                
+            ])
+            return action
